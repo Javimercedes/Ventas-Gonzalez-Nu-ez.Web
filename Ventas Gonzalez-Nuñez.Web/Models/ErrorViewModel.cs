771455279a8149582336956ca66fc44fefe63832
javiMercedes <Jgonzalez20190328@ucsd.edu.do>
2021-03-07 18:47:31 -0400

    Add project files.

---
+++ Ventas Gonzalez-Nuñez.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ventas_Gonzalez_Nuñez.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
