d77e65f1a04e48c103208c3b5f14c15142e95eef
javiMercedes <Jgonzalez20190328@ucsd.edu.do>
2021-03-07 20:12:24 -0400

    add.Ventas.Web

---
+++ Ventas Gonzalez-Nuñez.Web/Models/Customers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ventas_Gonzalez_Nuñez.Web.Models
+{
+    public class Customers
+    {
+    }
+}
---
+++ Ventas Gonzalez-Nuñez.Web/Models/Measurement Units.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ventas_Gonzalez_Nuñez.Web.Models
+{
+    public class Measurement_Units
+    {
+
+    }
+}
